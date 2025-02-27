d4a1f193260678ba78acaab360e25e62a716d61c
Angele Barzilai <abarzila@z4r4p1.42lyon.fr>
2025-02-27 17:31:46 +0100

    working on the local storage

--- cell03/ex03/todo.js
+++ cell03/ex03/todo.js
@@ -1,17 +1,19 @@
+let key = Math.random();
 
 function new_todo()
 {
     let todo = prompt("To do :");
     if (todo)
     {
+        key = key + 1;
         const node = document.createElement("div");
         node.innerHTML = todo;
         document.getElementById("node-list").appendChild(node);
         node.classList.add("node")
         node.addEventListener("click", destroy);
-        console.log(todo);
+        node.setAttribute("id", `${key}`);
 
-        localStorage.setItem("node-content", todo);
+        localStorage.setItem(`key-${key}`, todo);
         console.log(localStorage);
     }
 }
@@ -20,16 +22,28 @@ function destroy(event)
 {
     if (confirm("Delete this task ?"))
     {
+        let keyNumber = Number(event.target.getAttribute("id"));
+        console.log(keyNumber);
+        localStorage.removeItem(`key-${keyNumber}`);
         event.target.remove();
     }
 }
+// const items = {...localStorage};
+// console.log(items);
 
-// function loadLocalStorage()
-// {
-//     let savedContent = localStorage.getItem("node-content");
-//     if (savedContent)
-//     {
+window.onload = setTasks();
 
-//     }
-// }
-
+function setTasks()
+{
+    console.log("hello");
+    const items = {...localStorage};
+    let len = items.length;
+    let i = 0;
+    while (i <= len)
+    {
+        console.log("hi");
+        console.log(items[i]);
+        i++;
+    }
+    console.log(items);
+}
